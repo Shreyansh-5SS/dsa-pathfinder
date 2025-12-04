a8f97c18bcec7e974b8766c3d9da5f309559496d
Shreyansh Singh <singhshreyansh0505@gmail.com>
2025-12-04 19:20:45 +0530

    Initial Spring Boot project from Spring Initializr

---
+++ src/main/java/com/shreyansh/dsa_pathfinder/DsaPathfinderApplication.java
@@ -0,0 +1,13 @@
+package com.shreyansh.dsa_pathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaPathfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaPathfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyansh/dsa_pathfinder/DsaPathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyansh.dsa_pathfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaPathfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
