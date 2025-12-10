e23a63fb1f5898969a37a87e656d1621572739bf
Shreyansh Singh <singhshreyansh0505@gmail.com>
2025-12-11 01:37:01 +0530

    feat: add base spring scaffold and home page

---
+++ spring-backend/src/main/java/com/shreyansh/dsapathfinder/DsaPathfinderApplication.java
@@ -0,0 +1,13 @@
+package com.shreyansh.dsapathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaPathfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaPathfinderApplication.class, args);
+    }
+
+}
